96e54b3073c7f3b432c29f1bd6ee38c3123deac4
Eduard Saryan <eduardevops@users.noreply.github.com>
2020-03-28 00:41:46 +0400

    Update backup/restore scripts

--- backup/db_backup.sh
+++ backup/db_backup.sh
@@ -1,12 +1,13 @@
 #!/bin/bash
 
 # Cron jobs
-# * * * * *    /bin/bash jira8-mariadb/backup/db_backup.sh >> /var/log/db_backup-cron.log
+# * * * * *    /bin/bash /path/to/dockerized-jira8-postgresql/backup/db_backup.sh >> /var/log/db_backup-cron.log
 # # If you do not have a clear understanding how to configure date in crontab or you just feel too lazy to do so, use https://crontab.guru
 
 
 # Getting current date
 now=$(date +"%d-%b-%Y")
 
+source /path/to/dockerized-jira8-postgresql/.env.db
 
-docker exec jira-db pg_dump -U postgres <database_name> > /path/to/backup-folder/dump_${now}.sql
+docker exec jira-db pg_dump -U $POSTGRES_USER $POSTGRES_DB > /path/to/backup-folder/pgdump_${now}.sql
--- backup/db_restore.sh
+++ backup/db_restore.sh
@@ -3,4 +3,4 @@
 source /opt/dockerized-jira8-mysql/.env.db
 
 # Restore Stackpost DB
-docker exec -i <postgres_container_name> psql -U postgres -d <database_name> < backup.sql
+docker exec  jira-db psql -U $POSTGRES_USER -d $POSTGRES_DB < /path/to/bkup-file/pgdump.sql
