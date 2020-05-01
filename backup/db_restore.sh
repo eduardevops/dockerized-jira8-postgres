374cbb30dd028d65281773aca15f280711dc30e3
Eduard Saryan <eduardevops@users.noreply.github.com>
2020-05-01 20:13:36 +0400

    Update scripts

--- backup/db_restore.sh
+++ backup/db_restore.sh
@@ -1,6 +1,5 @@
 #!/bin/bash
 
+# Restore DB
 source /opt/dockerized-jira8-mysql/.env.db
-
-# Restore Stackpost DB
 docker exec  jira-db psql -U $POSTGRES_USER -d $POSTGRES_DB < /path/to/backup-file/pgdump.sql
--- backup/jira_backup.sh
+++ backup/jira_backup.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 
 # Cronjob example
-# * * * * *    /bin/bash jira8.2-mariadb/backup/jira_backup.sh >> /var/log/db_backup-cron.log
+# * * * * *    /bin/bash /path/to/jira_backup.sh >> /var/log/jira-cron.log
 
 
-# Get current date
-now=$(date +"%d.%b.%Y"-"%H:%M")
+# Get current date.
+now=$(date +"%d-%b-%Y")
 
-# Copy XML files from container to host
+# Copy XML files from container to host.
 docker cp jira-web:/var/atlassian/jira/export/. /backup/jira-xml/
